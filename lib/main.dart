857531d7308dc4c717121d69f6d7bde5fdbe2c69
Sahraoui Larbi <sahraouilarbi@gmail.com>
2021-09-21 16:23:27 +0100

    installation de flutter_riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:cartanawc_app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(CartanaApp());
+  runApp(
+    ProviderScope(
+      child: CartanaApp(),
+    ),
+  );
 }
