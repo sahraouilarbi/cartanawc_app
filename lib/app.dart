1b0693f0241abc531029f295db159af9d501f0ae
Sahraoui Larbi <sahraouilarbi@gmail.com>
2021-10-17 09:08:43 +0100

    suppression de commentaires unitiles

--- lib/app.dart
+++ lib/app.dart
@@ -10,30 +10,24 @@ import 'package:overlay_support/overlay_support.dart';
 import 'package:provider/provider.dart';
 
 class CartanaApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           create: (context) => LoaderProvider(),
-          //child: const BasePage(),
         ),
         ChangeNotifierProvider(
           create: (context) => ProductProvider(),
-          //child: const ProductPage(),
         ),
         ChangeNotifierProvider(
           create: (context) => CartProvider(),
-          //child: ProductDetails(),
         ),
         ChangeNotifierProvider(
           create: (context) => OrderProvider(),
-          //child: const OrdersPage(),
         ),
         ChangeNotifierProvider(
           create: (context) => AuthProvider(),
-          //child: const MyAccountPage(),
         ),
         // ChangeNotifierProvider(
         //   create: (context) => CustomerProvider(),
@@ -51,11 +45,6 @@ class CartanaApp extends StatelessWidget {
             primarySwatch: Colors.blue,
           ),
           home: const HomePage(),
-          //home: const SplashScreenPage(),
-          // home: const DefaultTabController(
-          //   length: 3,
-          //   child: HomePage(),
-          // ),
         ),
       ),
     );
