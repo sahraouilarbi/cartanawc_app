ae0477cbd6b1c29a90d3d337ee877a9d92e8e0be
Sahraoui Larbi <sahraouilarbi@gmail.com>
2022-06-02 11:05:43 +0100

    creation de constante String pour KEY

--- lib/core/prefs/app_prefs.dart
+++ lib/core/prefs/app_prefs.dart
@@ -17,70 +17,75 @@ abstract class AppPreferences {
 class AppPreferencesImpl implements AppPreferences {
   AppPreferencesImpl(this._sharedPreferences);
   final SharedPreferences _sharedPreferences;
+  static const String prefsKeyUsername = 'PREFS_KEY_USERNAME';
+  static const String prefsKeyPassword = 'PREFS_KEY_PASSWORD';
+  static const String prefsKeyToken = 'PREFS_KEY_TOKEN';
+  static const String prefsKeyIsUserLoggedIn = 'PREFS_KEY_IS_USER_LOGGED_IN';
+  static const String prefsKeyUserID = 'PREFS_KEY_USER_ID';
 
   // Save Username
   @override
   Future<void> setUsername(String username) async {
-    _sharedPreferences.setString('PREFS_KEY_USERNAME', username);
+    _sharedPreferences.setString(prefsKeyUsername, username);
   }
 
   // Get Username
   @override
   Future<String> getUsername() async {
-    return _sharedPreferences.getString('PREFS_KEY_USERNAME') ?? '';
+    return _sharedPreferences.getString(prefsKeyUsername) ?? '';
   }
 
   // Save Password
   @override
   Future<void> setPassword(String password) async {
-    _sharedPreferences.setString('PREFS_KEY_USERNAME', password);
+    _sharedPreferences.setString(prefsKeyPassword, password);
   }
 
   // Get Password
   @override
   Future<String> getPassword() async {
-    return _sharedPreferences.getString('PREFS_KEY_USERNAME') ?? '';
+    return _sharedPreferences.getString(prefsKeyPassword) ?? '';
   }
 
   // Save Token
   @override
   Future<void> setUserToken(String token) async {
-    _sharedPreferences.setString('PREFS_KEY_TOKEN', token);
+    _sharedPreferences.setString(prefsKeyToken, token);
   }
 
   // Get Token
   @override
   Future<String> getUserToken() async {
-    return _sharedPreferences.getString('PREFS_KEY_TOKEN') ?? '';
+    return _sharedPreferences.getString(prefsKeyToken) ?? '';
   }
 
   // Set IsUserLoggedIn to true
   @override
   Future<void> setIsUserLoggedIn() async {
-    _sharedPreferences.setBool('PREFS_KEY_IS_USER_LOGGED_IN', true);
+    _sharedPreferences.setBool(prefsKeyIsUserLoggedIn, true);
   }
 
   // Get IsUserLoggedIn
   @override
   Future<bool> isUserLoggedIn() async {
-    return _sharedPreferences.getBool('PREFS_KEY_IS_USER_LOGGED_IN') ?? false;
+    return _sharedPreferences.getBool(prefsKeyIsUserLoggedIn) ?? false;
   }
 
   // LOGOUT
   @override
   Future<void> logout() async {
-    _sharedPreferences.remove('PREFS_KEY_IS_USER_LOGGED_IN');
+    _sharedPreferences.remove(prefsKeyIsUserLoggedIn);
   }
 
   // Set user id
   @override
   Future<void> setUserId(int userId) async {
-    _sharedPreferences.setInt('PREFS_KEY_USER_ID', userId);
+    _sharedPreferences.setInt(prefsKeyUserID, userId);
   }
 
   //Get user id
   @override
   Future<int> getUserId() async {
-    return _sharedPreferences.getInt('PREFS_KEY_USER_ID');
+    return _sharedPreferences.getInt(prefsKeyUserID);
   }
 }
